aa6cb1fe6ca8f5530d536dc34a16d995e823caf6
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2012-10-22 13:54:41 +0200

    First commit
    Made a really simple main.py with 2 classes. Realy just arbitrary right
    now.

---
+++ main.py
@@ -0,0 +1,59 @@
+import direct.directbase.DirectStart
+from direct.showbase.DirectObject import DirectObject
+from panda3d.core import *
+from direct.actor.Actor import Actor
+from direct.task import Task
+
+class World(DirectObject):
+	
+	def __init__(self):
+		# Set background color
+		base.setBackgroundColor(0.5, 0.5, 0.5, 1)
+
+		# Models directory
+		self.modelsDir = "models/"
+
+		# Main game node
+		self.mainNode = render.attachNewNode('mainNode')	
+
+		# Setup directional light
+		dlight = DirectionalLight('dlight')
+		dlight.setColor(VBase4(1, 1, 0.5, 1))
+
+		dlightNode = self.mainNode.attachNewNode(dlight)
+		dlightNode.setHpr(0, -60, 0)
+		self.mainNode.setLight(dlightNode)		
+
+		# Setup environment (plane)
+		plane = loader.loadModel(self.modelsDir + "grass_plane.egg")
+
+		plane.setPos(0, 0, 0)
+		plane.setHpr(0, -90, 0)
+		plane.setScale(5)
+
+		plane.reparentTo(self.mainNode)
+
+class Unit():
+
+	def __init__(self):
+		print("Unit class instantiated")
+		self.initAttributes()
+
+	def initAttributes(self):
+		self.Strength = 0
+		self.Constitution = 0
+		self.Dexterity = 0
+
+		self.Damage = 0 # affected by strength
+		self.HealthPoints = 0 # affected by constitution
+		self.Armor = 0 
+
+	def initLevel(self):
+		self.Level = 1
+
+
+
+app = World()
+run()
+
+
