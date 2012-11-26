05949211064e1fb1a031257766068620cd8fdff6
Jan Michael Jensen <mateusdarkblade@gmail.com>
2012-11-26 11:04:05 +0100

    Quest class updated
    Does not fuck shit up anymore

--- src/quest.py
+++ src/quest.py
@@ -1,6 +1,6 @@
 from direct.gui.DirectGui import *
-from direct.gui.OnscreenText import OnscreenText
-from panda3d.core import TextNode
+from direct.gui.OnscreenText import *
+from panda3d.core import *
 
 #from panda3d.core import TextNode
 class Quest:
@@ -11,14 +11,14 @@ class Quest:
 
 
 	def addQuest(text):
-		self.textObject = OnscreenText(text=text, 
+		self.textQuest = OnscreenText(text=text, 
 									  pos=(-1,-0.95), 
-									  scale = 0.07, 
+									  scale=0.07, 
 									  fg =(1,0.3,0.8,1), 
 									  bg=(0.0,0.0,0.0,1.0),
-									  mayChange = 1)
+									  mayChange=1)
 	
-	addQuest('Get to the exit')
+		addQuest('Get to the exit')
 	#text_Obj = "Find the exit"
 	#textObject = OnscreenText(text=text_Obj, font = font, pos = (-1,-0.85),scale = 0.07,
 	#							fg =(1,0.5,0.5,1), bg=(0.0,0.0,0.0,1.0))
