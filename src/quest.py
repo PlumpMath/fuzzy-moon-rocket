445653a67b903c38fc76c5ea6a255078f69269dc
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2012-11-26 15:16:41 +0100

    Fixed quest class enough to make it compilable

--- src/quest.py
+++ src/quest.py
@@ -1,24 +1,24 @@
 from direct.gui.DirectGui import *
 from direct.gui.OnscreenText import OnscreenText
-from panda3d.core import TextNode
+from panda3d.core import *
 
 #from panda3d.core import TextNode
 class Quest:
 
 	def __init__(self):
 		print("Quest class instantiated")
-		self.addQuest()
+		self.addQuest('Get to the exit')
 
 
-	def addQuest(text):
+	def addQuest(self, text):
 		self.textObject = OnscreenText(text=text, 
 									  pos=(-1,-0.95), 
 									  scale = 0.07, 
 									  fg =(1,0.3,0.8,1), 
 									  bg=(0.0,0.0,0.0,1.0),
-									  mayChange = 1)
+									  mayChange=1)
 	
-	addQuest('Get to the exit')
+	#self.addQuest('Get to the exit')
 	#text_Obj = "Find the exit"
 	#textObject = OnscreenText(text=text_Obj, font = font, pos = (-1,-0.85),scale = 0.07,
 	#							fg =(1,0.5,0.5,1), bg=(0.0,0.0,0.0,1.0))
