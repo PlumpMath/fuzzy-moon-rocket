72fa1636dcd3a67e135d1d39bb2e7ebd225aba64
Jan Michael Jensen <mateusdarkblade@gmail.com>
2012-11-26 10:19:08 +0100

    Changing quest class
    not done yet

--- src/quest.py
+++ src/quest.py
@@ -3,16 +3,22 @@ from direct.gui.OnscreenText import OnscreenText
 from panda3d.core import TextNode
 
 #from panda3d.core import TextNode
-class Quest():
-	#font = loader.loadFont("cmss12")
-	font = TextNode.getDefaultFont()
+class Quest:
 
-	#def addQuest(pos, msg):
+	def __init__(self):
+		print("Quest class instantiated")
+		self.addQuest()
+
+
+	def addQuest(text):
+		self.textObject = OnscreenText(text=text, 
+									  pos=(-1,-0.95), 
+									  scale = 0.07, 
+									  fg =(1,0.3,0.8,1), 
+									  bg=(0.0,0.0,0.0,1.0),
+									  mayChange = 1)
 	
-	text_Ins = "Get to the exit"
-	textObject = OnscreenText(text=text_Ins,font = font, pos=(-1,-0.95), scale = 0.07, 
-								fg =(1,0.3,0.8,1), bg=(0.0,0.0,0.0,1.0))
-	
+	addQuest('Get to the exit')
 	#text_Obj = "Find the exit"
 	#textObject = OnscreenText(text=text_Obj, font = font, pos = (-1,-0.85),scale = 0.07,
 	#							fg =(1,0.5,0.5,1), bg=(0.0,0.0,0.0,1.0))
